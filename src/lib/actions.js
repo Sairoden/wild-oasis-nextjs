c4c76c0dbd334990437c83d37bd34846ae8d2277
Sairoden <odingandarosa@gmail.com>
2025-01-14 13:47:43 +0800

    feat: update Reservation--Delete booking

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -8,6 +8,7 @@ import { auth, signIn, signOut } from "./auth";
 
 // SUPABASE
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export const signInAction = async () => {
   await signIn("google", { redirectTo: "/account" });
@@ -43,4 +44,27 @@ export const updateGuest = async formData => {
   }
 
   revalidatePath("/account/profile");
+};
+
+export const deleteReservation = async bookingId => {
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map(booking => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  console.log("MY BOOK ID", bookingId);
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 };
